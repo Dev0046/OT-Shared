b4c3fb4bf02198b7311bc427aeca7d707c8f01ec
Dev Kumar Gautam <151554711+Dev0046@users.noreply.github.com>
2024-07-14 01:50:14 +0530

    Update Dependency.groovy

--- src/org/java/Dependency.groovy
+++ src/org/java/Dependency.groovy
@@ -2,6 +2,11 @@ package org.java
 
 def call() {
     sh '''
-        ${DEPENDENCY_CHECK_HOME}/bin/dependency-check.sh --scan . --format ALL
+        def trivyOutput = sh(script: 'trivy --exit-code 1 --severity HIGH --no-progress /path/to/your/app', returnStdout: true).trim()
+                    echo "Trivy Scan Results:\n${trivyOutput}"
+                    
+                    // You can optionally fail the build based on Trivy results
+                    if (trivyOutput.contains('High') || trivyOutput.contains('Critical')) {
+                        error 'Security vulnerabilities found'L
     '''
 }
