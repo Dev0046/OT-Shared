a17790a29c91d95d7592c353eafd876eee271105
Dev Kumar Gautam <151554711+Dev0046@users.noreply.github.com>
2024-07-14 00:40:42 +0530

    Update frontend.groovy

--- vars/frontend.groovy
+++ vars/frontend.groovy
@@ -5,6 +5,6 @@ import org.java.CredScanning
 def checkoutgit(String gitUrl, String branch = 'main') {
     new Checkout().call(gitUrl, branch)
 }
-def callCodeCompilation() {
-    new CodeCompilation().call()
+def CredScanning() {
+    new CredScanning().call()
 }
