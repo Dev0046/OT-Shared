1f443d31c74878b0e0f9534527d69450993ede3f
Dev Kumar Gautam <151554711+Dev0046@users.noreply.github.com>
2024-07-13 23:44:07 +0530

    Update FrontendCI.groovy

--- template/FrontendCI.groovy
+++ template/FrontendCI.groovy
@@ -3,3 +3,6 @@ import org.java.Checkout
 def checkoutgit(String gitUrl, String branch = 'main') {
     new Checkout().call(gitUrl, branch)
 }
+def callCodeCompilation() {
+    new CodeCompilation().call()
+}
