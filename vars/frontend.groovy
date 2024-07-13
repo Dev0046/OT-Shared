ebe61cd208f52694befff60270b6df83a36a9691
Dev Kumar Gautam <151554711+Dev0046@users.noreply.github.com>
2024-07-14 01:45:17 +0530

    Update frontend.groovy

--- vars/frontend.groovy
+++ vars/frontend.groovy
@@ -1,6 +1,7 @@
 import org.java.Checkout
 import org.java.CredScanning
 import org.java.Sonarqube
+import org.java.Dependency
 
 def checkoutgit(String gitUrl, String branch = 'main') {
     new Checkout().call(gitUrl, branch)
