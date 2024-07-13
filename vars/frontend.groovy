fbb9d1c89922296ed74cfb93c02dab749a4c3ccd
Dev Kumar Gautam <151554711+Dev0046@users.noreply.github.com>
2024-07-14 00:46:39 +0530

    Update frontend.groovy

--- vars/frontend.groovy
+++ vars/frontend.groovy
@@ -1,10 +1,13 @@
 import org.java.Checkout
 import org.java.CredScanning
-
+import org.java.sonarqube
 
 def checkoutgit(String gitUrl, String branch = 'main') {
     new Checkout().call(gitUrl, branch)
 }
 def CredScanning() {
     new CredScanning().call()
+}
+def callsonarqube() {
+    new sonarqube().call()
 }
