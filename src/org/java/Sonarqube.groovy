47834b73bcb2098ee9ecc49e7b56d7eead7bacd6
Dev Kumar Gautam <151554711+Dev0046@users.noreply.github.com>
2024-07-14 02:33:33 +0530

    Update Sonarqube.groovy

--- src/org/java/Sonarqube.groovy
+++ src/org/java/Sonarqube.groovy
@@ -4,11 +4,7 @@ def call() {
     def scannerHome = tool name: 'sonar', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
     withSonarQubeEnv('sonar') {
         sh """
-        ${scannerHome}/bin/sonar-scanner \
-        -Dsonar.projectKey=frontend 
-       -Dsonar.sources=. 
-       -Dsonar.host.url=http://54.84.78.174:9000 
-       -Dsonar.token=sqp_13bafe53f7ecf3f7e95f6095b12a661104111fbb
+        ${scannerHome}/bin/sonar-scanner
         """
     }
 }
