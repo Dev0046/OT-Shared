2e5b2b6c98a7601a169b7f21fa95fdade40bd77c
Dev Kumar Gautam <151554711+Dev0046@users.noreply.github.com>
2024-07-14 01:53:35 +0530

    Update Dependency.groovy

--- src/org/java/Dependency.groovy
+++ src/org/java/Dependency.groovy
@@ -3,13 +3,6 @@ package org.java
 def call() {
     // Using triple quotes for multi-line shell script
     sh '''
-        trivyOutput=$(trivy --exit-code 1 --severity HIGH --no-progress /path/to/your/app)
-        echo "Trivy Scan Results:"
-        echo "${trivyOutput}"
-        
-        # Check if Trivy found High or Critical vulnerabilities
-        if [[ "${trivyOutput}" == *"High"* || "${trivyOutput}" == *"Critical"* ]]; then
-            error "Security vulnerabilities found"
-        fi
+       sh 'trivy repo https://github.com/OT-MICROSERVICES/frontend.git --format json -o trivy-license-report.json'
     '''
 }
