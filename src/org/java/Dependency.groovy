2594fc64161c544ba275e458675f7a6529b006cd
Dev Kumar Gautam <151554711+Dev0046@users.noreply.github.com>
2024-07-14 02:09:08 +0530

    Update Dependency.groovy

--- src/org/java/Dependency.groovy
+++ src/org/java/Dependency.groovy
@@ -4,6 +4,5 @@ def call() {
     // Using triple quotes for multi-line shell script
     sh '''
         sh 'trivy repo https://github.com/OT-MICROSERVICES/frontend.git --format json -o trivy-license-report.json'
-        archiveArtifacts artifacts: 'trivy-license-report.json', allowEmptyArchive: true
     '''
 }
