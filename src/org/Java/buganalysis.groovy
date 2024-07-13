e7c61eb80949997cd215b7ceae0800a4e32dea97
Dev Kumar Gautam <151554711+Dev0046@users.noreply.github.com>
2024-07-13 23:24:45 +0530

    Update buganalysis.groovy

--- src/org/Java/buganalysis.groovy
+++ src/org/Java/buganalysis.groovy
@@ -1,1 +1,13 @@
-aa
+package org.java
+
+def call() {
+    def scannerHome = tool name: 'sonar', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+    withSonarQubeEnv('sonar') {
+        sh """
+              -Dsonar.projectKey=sonar 
+              -Dsonar.projectName='sonar' 
+              -Dsonar.host.url=http://54.84.78.174
+              -Dsonar.token=sqp_f4b436a174bd4bc43f833a85fa392b290453c7da
+        """
+    }
+}
